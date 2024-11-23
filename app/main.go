be17ef1cd88c594313e1e68e3b6485ea322d9b74
nshm <asrgmt24@gmail.com>
2024-11-23 12:24:28 +0900

    fix log

--- app/main.go
+++ app/main.go
@@ -10,8 +10,8 @@ func main() {
 	fmt.Println("Starting server on port :9000")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to my website!")
-		fmt.Println("Successfully served request " + time.Now().Format(time.RFC3339))
 	})
 
+	fmt.Println("Successfully served request " + time.Now().Format(time.RFC3339))
 	http.ListenAndServe(":9000", nil)
 }
