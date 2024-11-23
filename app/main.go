d7be724c6fe521e0967db92656ac5da4b991823b
nshm <asrgmt24@gmail.com>
2024-11-23 12:16:54 +0900

    add log

--- app/main.go
+++ app/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
 	fmt.Println("Starting server on port :9000")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to my website!")
+		fmt.Println("Successfully served request " + time.Now().Format(time.RFC3339))
 	})
 
 	http.ListenAndServe(":9000", nil)
